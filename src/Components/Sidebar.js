064ce900b30475f63ff04f6a295059af02950ebd
Bonin <francesco.bonin@vertivco.com>
2023-11-15 08:48:38 +0100

    Added conditional formatting to sidebar

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -11,10 +11,14 @@ import VD from '../img/vd.jpg'
 
 const Sidebar = () => {
 
+    let largeScreen = true
+    if (window.innerWidth < 1200)
+        largeScreen = false
+    
     let disclaimer = `Questo il libro per voi...se vi piace il nostro approccio alla montagna`
     return (
     <>
-      <div className="itemLeft sidebar">
+      <div className={`${'itemLeft'} ${largeScreen ? 'sidebar' : ''}`}>
           <div className="logotext">
               <p></p>
               <a target="_blank" href="http://www.vividolomiti.it/pubblicazioni/wild-dolomiti-3-detail" rel="noreferrer">
@@ -25,37 +29,37 @@ const Sidebar = () => {
             <div>
                 <ul className={`navbar-nav mr-auto`}>
                     <li className={`nav-item active`}>
-                        <NavLink id="home" className={`${'btn'} ${'btn-primary'} ${'buttonSide'}`} to="/alpago">Alpago</NavLink>
+                        <NavLink className={`${'btn'} ${'btn-primary'} ${'buttonSide'}`} to="/alpago">Alpago</NavLink>
                     </li><p></p>
                     <li className={`nav-item active`}>
-                        <NavLink id="loadlog" className={`${'btn'} ${'btn-primary'} ${'buttonSide'}`} to="/cadore">Cadore</NavLink>
+                        <NavLink className={`${'btn'} ${'btn-primary'} ${'buttonSide'}`} to="/cadore">Cadore</NavLink>
                     </li><p></p>
                     <li className={`nav-item active`}>
-                        <NavLink id="requestlog" className={`${'btn'} ${'btn-primary'} ${'buttonSide'}`} to="/carniche">Alpi Carniche</NavLink>
+                        <NavLink className={`${'btn'} ${'btn-primary'} ${'buttonSide'}`} to="/carniche">Alpi Carniche</NavLink>
                     </li><p></p>
                     <li className={`nav-item active`}>
-                        <NavLink id="requestlog" className={`${'btn'} ${'btn-primary'} ${'buttonSide'}`} to="/giulie">Alpi Giulie</NavLink>
+                        <NavLink className={`${'btn'} ${'btn-primary'} ${'buttonSide'}`} to="/giulie">Alpi Giulie</NavLink>
                     </li><p></p>
                     <li className={`nav-item active`}>
-                        <NavLink id="runtest" className={`${'btn'} ${'btn-primary'} ${'buttonSide'}`} to="/oltrepiave">Oltre Piave</NavLink>
+                        <NavLink className={`${'btn'} ${'btn-primary'} ${'buttonSide'}`} to="/oltrepiave">Oltre Piave</NavLink>
                     </li><p></p>
                     <li className={`nav-item active`}>
-                        <NavLink id="runtest" className={`${'btn'} ${'btn-primary'} ${'buttonSide'}`} to="/pale">Pale di San Martino</NavLink>
+                        <NavLink className={`${'btn'} ${'btn-primary'} ${'buttonSide'}`} to="/pale">Pale di San Martino</NavLink>
                     </li><p></p>
                     <li className={`nav-item active`}>
-                        <NavLink id="runtest" className={`${'btn'} ${'btn-primary'} ${'buttonSide'}`} to="/zoldano">Zoldano</NavLink>
+                        <NavLink className={`${'btn'} ${'btn-primary'} ${'buttonSide'}`} to="/zoldano">Zoldano</NavLink>
                     </li><p></p>
                     <li className={`nav-item active`}>
-                        <NavLink id="runtest" className={`${'btn'} ${'btn-primary'} ${'buttonSide'}`} to="/bellunesi">Dolomiti Bellunesi</NavLink>
+                        <NavLink className={`${'btn'} ${'btn-primary'} ${'buttonSide'}`} to="/bellunesi">Dolomiti Bellunesi</NavLink>
                     </li><p></p>
                     <li className={`nav-item active`}>
-                        <NavLink id="runtest" className={`${'btn'} ${'btn-primary'} ${'buttonSide'}`} to="/brenta">Dolomiti di Brenta</NavLink>
+                        <NavLink className={`${'btn'} ${'btn-primary'} ${'buttonSide'}`} to="/brenta">Dolomiti di Brenta</NavLink>
                     </li><p></p>
                     <li className={`nav-item active`}>
-                        <NavLink id="runtest" className={`${'btn'} ${'btn-primary'} ${'buttonSide'}`} to="/fassa">Fassa e Marmolada</NavLink>
+                        <NavLink className={`${'btn'} ${'btn-primary'} ${'buttonSide'}`} to="/fassa">Fassa e Marmolada</NavLink>
                     </li><p></p>
                     <li className={`nav-item active`}>
-                        <NavLink id="runtest" className={`${'btn'} ${'btn-primary'} ${'buttonSide'}`} to="/varie">Varie</NavLink>
+                        <NavLink className={`${'btn'} ${'btn-primary'} ${'buttonSide'}`} to="/varie">Varie</NavLink>
                     </li>
                 </ul>
             </div>
