6b3d0a674df5e5af16248ecafd799c3fddcefb82
Bonin <francesco.bonin@vertivco.com>
2023-11-24 10:21:22 +0100

    Resized home page picture in case of mobile screen.

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,13 +4,17 @@ import '../App.css'
 
 export default function home() {
 
+  let largeScreen = true
+  if (window.innerWidth < 1200)
+      largeScreen = false
+
 return (
   <div className='row App'>
     <div className='col-sm-2 colLeft'>
       <Sidebar />
     </div>
     <div className='col-sm-10 pictureHome'>
-      <img src={require('../img/zoldano/pelmetto.jpg')} alt='Palafavera dal Pelmetto'/>
+      <img src={require('../img/zoldano/pelmetto.jpg')} alt='Palafavera dal Pelmetto' className={`${largeScreen ? '' : 'pictureResize'}`}/>
     </div>  
   </div>                      
   );
